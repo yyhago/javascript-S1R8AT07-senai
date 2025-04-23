5f6de9b0e42ed856bae567144b09fb54044b575a
yyhago <yhago.felipe.teles@gmail.com>
2025-04-23 09:50:29 -0300

    improvements

--- challenge02/script.js
+++ challenge02/script.js
@@ -4,11 +4,14 @@ let numeroRandow = Math.ceil(Math.random()*10);
 
 for(let i = 0; i < 3; i++){
     inputUsuario = prompt("ACERTE O NÚMERO:");
+
     if (inputUsuario == numeroRandow){
         console.log(`VOCÊ GANHOU, o número randomico era ${numeroRandow}`);
         break;
-    } 
-    console.log("Opss... Você errou, tente novamente!");
+    }
+
+    inputUsuario < numeroRandow ? console.log("Opss... Você errou, tente novamente!\nDICA: O número é maior ") : console.log("Opss... Você errou, tente novamente!\nDICA: O número é menor ");
+
 }
 
 if (inputUsuario != numeroRandow){
